383816cb4f0704f7340f175f43863ef43b191a57
Morgarin <efim.gni@gmail.com>
2025-04-20 23:37:31 +0300

    first final commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -3,9 +3,12 @@ package daysteps
 import (
 	"errors"
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 	"time"
+
+	. "github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -18,18 +21,26 @@ const (
 func parsePackage(data string) (int, time.Duration, error) {
 	strings := strings.Split(data, ",")
 	if len(strings) != 2 {
-		return 0, 0, errors.New("not enough arguments: expected data")
+		log.Println(errors.New("parsePackage: len(strings) !=2"))
+		return 0, 0, errors.New("parsePackage: len(strings) !=2")
 	}
 	steps, err := strconv.Atoi(strings[0])
 	if err != nil {
-		return 0, 0, errors.New("cant convert steps(str) to int")
+		log.Println(err)
+		return 0, 0, errors.New("parsePackage: cant convert steps to string")
+	}
+	if steps < 1 {
+		log.Println(err)
+		return 0, 0, errors.New("parsePackage: steps <= 0")
 	}
 	duration, err := time.ParseDuration(strings[1])
 	if err != nil {
-		return 0, 0, errors.New("cant convert duration")
+		log.Println(err)
+		return 0, 0, errors.New("parsePackage: cant convert duration to time.Duration")
 	}
-	if steps <= 0 {
-		return 0, 0, errors.New("steps count: <= 0")
+	if duration <= time.Duration(0) {
+		log.Println(errors.New("DayActionInfo: duration <= 0"))
+		return 0, 0, errors.New("DayActionInfo: duration <= 0")
 	}
 	return steps, duration, err
 }
@@ -37,13 +48,23 @@ func parsePackage(data string) (int, time.Duration, error) {
 func DayActionInfo(data string, weight, height float64) string {
 	steps, duration, err := parsePackage(data)
 	if err != nil {
+		log.Println(err)
+		fmt.Println(err)
 		return ""
 	}
-	if steps <= 0 {
+	if steps < 1 {
+		log.Println(errors.New("DayActionInfo: steps <= 0"))
 		return ""
 	}
-	distance := stepLength * float64(steps)
-	distance = distance / float64(mInKm)
-	calories := WalkingSpentCalories(steps, weight, height, duration)
-	return fmt.Sprintf("Количество шагов: %d\n Дистанция составила %.2f км.\n Вы сожгли %.2f ккал.", steps, distance, calories)
+	distance := stepLength * float64(steps) / float64(mInKm)
+	calories, err := WalkingSpentCalories(steps, weight, height, duration)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	if duration <= time.Duration(0) {
+		log.Println(errors.New("DayActionInfo: duration <= 0"))
+		return ""
+	}
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", steps, distance, calories)
 }
--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -20,29 +20,37 @@ const (
 func parseTraining(data string) (int, string, time.Duration, error) {
 	strings := strings.Split(data, ",")
 	if len(strings) != 3 {
-		return 0, "", 0, errors.New("not enough arguments: expected data")
+		return 0, "", 0, errors.New("parseTraining: len(strings) != 3")
 	}
 	steps, err := strconv.Atoi(strings[0])
 	if err != nil {
 		return 0, "", 0, err
 	}
+	if steps < 1 {
+		return 0, "", 0, errors.New("parseTraining: steps <= 0")
+	}
 	duration, err := time.ParseDuration(strings[2])
 	if err != nil {
 		return 0, "", 0, err
 	}
-	return steps, strings[1], duration, nil
+	if duration <= 0 {
+		return 0, "", 0, errors.New("parseTraining: steps <= 0")
+	}
+	return steps, strings[1], duration, err
 }
 
 // Дистанция
 func distance(steps int, height float64) float64 {
 	stepLength := height * stepLengthCoefficient
 	return stepLength * float64(steps) / mInKm
-
 }
 
 // Средняя скорость
 func meanSpeed(steps int, height float64, duration time.Duration) float64 {
 	if duration <= 0 {
+		return 0
+	}
+	if steps < 1 {
 		return 0
 	}
 	return distance(steps, height) / duration.Hours()
@@ -51,25 +59,29 @@ func meanSpeed(steps int, height float64, duration time.Duration) float64 {
 // Вывод информации о тренировке
 func TrainingInfo(data string, weight, height float64) (string, error) {
 	steps, trainingType, duration, err := parseTraining(data)
+	if err != nil {
+		return "", err
+	}
 	switch trainingType {
 	case "Бег":
 		dist := distance(steps, height)
 		speed := meanSpeed(steps, height, duration)
 		calories, err := RunningSpentCalories(steps, weight, height, duration)
-		return fmt.Sprintf("Тип тренировки: %s\n Длительность: %.2f ч.\n Дистанция: %.2f км.\n Скорость: %.2f км/ч\n Сожгли калорий: %.2f\n", trainingType, duration.Hours(), dist, speed, calories), err
+		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration.Hours(), dist, speed, calories), err
 	case "Ходьба":
 		dist := distance(steps, height)
 		speed := meanSpeed(steps, height, duration)
 		calories, err := WalkingSpentCalories(steps, weight, height, duration)
-		return fmt.Sprintf("Тип тренировки: %s\n Длительность: %.2f ч.\n Дистанция: %.2f км.\n Скорость: %.2f км/ч\n Сожгли калорий: %.2f\n", trainingType, duration.Hours(), dist, speed, calories), err
+		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", trainingType, duration.Hours(), dist, speed, calories), err
+	default:
+		return "", errors.New("TrainingInfo: неизвестный тип тренировки")
 	}
-	return "", err
 }
 
 // Количество потраченных калорий при беге
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	if (steps <= 0) || (weight <= 0) || (height <= 0) || (duration <= 0) {
-		return 0, fmt.Errorf("not enough arguments: expected data")
+		return 0, errors.New("RunningSpentCalories: value <= 0")
 	}
 	durationInMinutes := float64(duration.Minutes())
 	return (weight * meanSpeed(steps, height, duration) * durationInMinutes) / minInH, nil
